978b829618f48ea38e06bbb35034ca4643764ee0
zmizung@gmail.com <zmizung@gmail.com>
2023-03-21 09:37:11 +0100

    Add project files.

---
+++ MIDI Editor/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿
+namespace MIDI_Editor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.hScrollBar1 = new System.Windows.Forms.HScrollBar();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.trackBar2 = new System.Windows.Forms.TrackBar();
+            this.vScrollBar1 = new System.Windows.Forms.VScrollBar();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(700, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Open File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(700, 70);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Save File";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.BackColor = System.Drawing.Color.Black;
+            this.panel1.Location = new System.Drawing.Point(12, 38);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(640, 370);
+            this.panel1.TabIndex = 2;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.Panel1_Paint);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(679, 311);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Minimum = 1;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(109, 45);
+            this.trackBar1.TabIndex = 4;
+            this.trackBar1.Value = 1;
+            this.trackBar1.Scroll += new System.EventHandler(this.TrackBar1_Scroll);
+            // 
+            // hScrollBar1
+            // 
+            this.hScrollBar1.LargeChange = 25;
+            this.hScrollBar1.Location = new System.Drawing.Point(12, 411);
+            this.hScrollBar1.Maximum = 10000;
+            this.hScrollBar1.Name = "hScrollBar1";
+            this.hScrollBar1.Size = new System.Drawing.Size(641, 17);
+            this.hScrollBar1.TabIndex = 5;
+            this.hScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.HScrollBar1_Scroll);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 12);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(641, 20);
+            this.textBox1.TabIndex = 6;
+            this.textBox1.Text = "Default (empty) MIDI file";
+            // 
+            // trackBar2
+            // 
+            this.trackBar2.Location = new System.Drawing.Point(679, 363);
+            this.trackBar2.Maximum = 127;
+            this.trackBar2.Minimum = 12;
+            this.trackBar2.Name = "trackBar2";
+            this.trackBar2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.trackBar2.Size = new System.Drawing.Size(109, 45);
+            this.trackBar2.TabIndex = 7;
+            this.trackBar2.Value = 12;
+            this.trackBar2.Scroll += new System.EventHandler(this.TrackBar2_Scroll);
+            // 
+            // vScrollBar1
+            // 
+            this.vScrollBar1.Location = new System.Drawing.Point(659, 38);
+            this.vScrollBar1.Maximum = 10000;
+            this.vScrollBar1.Name = "vScrollBar1";
+            this.vScrollBar1.Size = new System.Drawing.Size(17, 370);
+            this.vScrollBar1.TabIndex = 8;
+            this.vScrollBar1.Scroll += new System.Windows.Forms.ScrollEventHandler(this.VScrollBar1_Scroll);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.Items.AddRange(new object[] {
+            "1/64",
+            "1/32",
+            "1/16",
+            "1/8",
+            "1/4",
+            "1/2",
+            "1 Bar",
+            "2 Bars",
+            "4 Bars"});
+            this.comboBox1.Location = new System.Drawing.Point(692, 118);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(83, 21);
+            this.comboBox1.TabIndex = 9;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.ComboBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.vScrollBar1);
+            this.Controls.Add(this.trackBar2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.hScrollBar1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.HScrollBar hScrollBar1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TrackBar trackBar2;
+        private System.Windows.Forms.VScrollBar vScrollBar1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ MIDI Editor/Form1.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using System.Windows.Forms;
+using NAudio.Midi;
+
+namespace MIDI_Editor
+{
+    public partial class Form1 : Form
+    {
+        public static string sDefaultPath = Directory.GetCurrentDirectory() + "/New MIDI file.mid";
+        public static string sSelectedPath;
+
+        public MidiFile mf = new MidiFile(sDefaultPath, true);
+
+        public float pixelsPerTick = 0.5F;
+        public float sph = 12;
+        //sph = slots per height (počet políček na výšku panelu)
+        public int noteSlotSize = 16;
+        public int xOffset = 0;
+        public int yOffset = 5000;
+        // noteSlotSize = 16 znamená, že jedno okénko gridu značí čtvrťovou notu (quarter note)
+        // respektive 16 1/64 not (nejmenších možných)
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            hScrollBar1.Value = xOffset;
+            vScrollBar1.Value = yOffset;
+            trackBar1.Value = (int)(pixelsPerTick * 100);
+            trackBar2.Value = (int)sph;
+            comboBox1.SelectedIndex = 4;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "MIDI files (*.mid)|*.mid|All files (*.*)|*.*",
+                RestoreDirectory = true
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    sSelectedPath = openFileDialog.FileName;
+                    mf = new MidiFile(openFileDialog.FileName, true);
+                    panel1.Refresh();
+                    textBox1.Text = openFileDialog.FileName;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog
+            {
+                Filter = "MIDI files (*.mid)|*.mid|All files (*.*)|*.*",
+                RestoreDirectory = true
+            };
+
+            if (saveFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                MidiFile.Export(saveFileDialog.FileName, mf.Events);
+            }
+        }
+
+        private void Panel1_Paint(object sender, PaintEventArgs e)
+        {
+            e.Graphics.Clear(Color.Black);
+
+            using (Pen penW = new Pen(Color.White))
+            using (Pen penG = new Pen (Color.Gray))
+            {
+                float DeltaTicksPer64thNote = mf.DeltaTicksPerQuarterNote / 16;
+                float pixelsPerSlot = DeltaTicksPer64thNote * pixelsPerTick * noteSlotSize;
+                float spw = panel1.Width / pixelsPerSlot;
+                // SPW = slots per width (počet políček na šířku panelu)
+                float xSlotsZoomedOut = (panel1.Width / (float)(mf.DeltaTicksPerQuarterNote / 100F * noteSlotSize / 16));
+                xOffset = (int)(xSlotsZoomedOut * hScrollBar1.Value / 10000 * pixelsPerSlot);
+                float xOffsetSlots = (float)Math.Floor((double)(xOffset / pixelsPerSlot));
+                float xOffsetPx = xOffset - (xOffsetSlots * pixelsPerSlot);
+
+                float pixelsPerPitch = panel1.Height / sph;
+                float ySlotsZoomedOut = 127;
+                yOffset = (int)(ySlotsZoomedOut * vScrollBar1.Value / 10000 * (pixelsPerPitch));
+                float yOffsetSlots = (float)Math.Floor((double)yOffset / (pixelsPerPitch));
+                float yOffsetPx = yOffset - (yOffsetSlots * (pixelsPerPitch));
+
+                //tohle jsem si psal sám, je to asi poznat tím jak je to nepřehledný
+
+                if (sph > 24)
+                {
+                    for (int i = 0; i <= (int)sph; i++)
+                    {
+                        e.Graphics.DrawLine(penG, 50, 12 * i * (pixelsPerPitch), panel1.Width, 12 * i * (pixelsPerPitch));
+                        e.Graphics.DrawLine(penW, 0, 12 * i * (pixelsPerPitch), 50, 12 * i * (pixelsPerPitch));
+                    }
+                }
+                else
+                {
+                    for (int i = 0; i <= (int)sph; i++)
+                    {
+                        e.Graphics.DrawLine(penG, 50, i * (pixelsPerPitch) + (pixelsPerPitch - yOffsetPx), panel1.Width, i * (pixelsPerPitch) + ((pixelsPerPitch) - yOffsetPx));
+                        e.Graphics.DrawLine(penW, 0, i * (pixelsPerPitch) + (pixelsPerPitch - yOffsetPx), 50, i * (pixelsPerPitch) + ((pixelsPerPitch) - yOffsetPx));
+                    }
+                }
+
+                e.Graphics.DrawLine(penW, 50, 0, 50, panel1.Height);
+
+                if (pixelsPerTick * noteSlotSize >= 0.9)
+                // prostě kontroluje, aby na sebe nebyly sloupce gridu moc nahuštěný
+                {
+                    
+                    for (int i = 0; i <= (spw + 1); i++)
+                    {
+                        e.Graphics.DrawLine(penG, 50 + i * pixelsPerSlot + (pixelsPerSlot - xOffsetPx), 0, 50 + i * pixelsPerSlot + (pixelsPerSlot - xOffsetPx), panel1.Height);
+                    }
+                }
+            }
+
+            foreach (var track in mf.Events)
+            {
+                foreach (var midiEvent in track)
+                {
+                    if (MidiEvent.IsNoteOn(midiEvent))
+                    {
+                        NoteOnEvent noteOnEvent = (NoteOnEvent)midiEvent;
+
+                        int x = (int)(noteOnEvent.AbsoluteTime * pixelsPerTick);
+                        int y = (int)(panel1.Height / sph * noteOnEvent.NoteNumber);
+                        int width = (int)(noteOnEvent.NoteLength * pixelsPerTick);
+                        int height = (int)(panel1.Height / sph);
+                        e.Graphics.FillRectangle(Brushes.Green, 51 + x - xOffset, 1 + y - yOffset, width, height);
+                    }
+                }
+            }
+        }
+
+        private void TrackBar1_Scroll(object sender, EventArgs e)
+        {
+            pixelsPerTick = trackBar1.Value / 100f;
+            panel1.Refresh();
+        }
+
+        private void TrackBar2_Scroll(object sender, EventArgs e)
+        {
+            sph = trackBar2.Value;
+            panel1.Refresh();
+        }
+
+        private void HScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            panel1.Refresh();
+        }
+
+        private void VScrollBar1_Scroll(object sender, ScrollEventArgs e)
+        {
+            panel1.Refresh();
+        }
+
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {            
+            noteSlotSize = 1 * (int)Math.Pow(2, comboBox1.SelectedIndex);
+            panel1.Refresh();
+        }
+    }
+}
